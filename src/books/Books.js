417554c41d39d30eda71d8284a0e4ee941ba1b23
Vinny Matticoli <Vinny@Vinnys-MacBook-Pro.local>
2020-02-19 18:21:13 -0500

    init project

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Books(){
+    return (
+        <div className="Books">
+            <div className="lander">
+                <h2>Books</h2>
+                <p>Welcome To The Book Section.</p>
+            </div>
+        </div>
+    );
+}
