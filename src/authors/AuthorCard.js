0cf14de716b31390e7d13e46766f2ce939f5e88f
Vinny Matticoli <Vinny@vinnys-mbp.home>
2020-02-26 03:47:32 -0500

    project init

--- src/authors/AuthorCard.js
+++ src/authors/AuthorCard.js
@@ -5,11 +5,11 @@ import { Card, Button } from "react-bootstrap";
 export function AuthorCard(props){
     return (
         <Card style={{width: "16em" }}>
-            <Card.Img variant="top" src="https://via.placeholder.com/150"  />
+            <Card.Img variant="top" src={props.author.image}  />
             <Card.Body>
                 <Card.Title>{props.author.firstName} {props.author.lastName} </Card.Title>
                 <Card.Text>Author Age: {props.author.age}</Card.Text>
-                <Button variant="warning">Add to Cart</Button>
+                <Button variant="warning">Click for nothing lol</Button>
                 <br></br>
             </Card.Body>
         </Card>
--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -11,30 +11,48 @@ export default class Authors extends Component{
                         id: 1,
                         firstName: "Shel",
                         lastName: "Silverstein",
-                        age: "68"
+                        age: "68",
+                        image: "https://upload.wikimedia.org/wikipedia/en/8/8f/Ssilverstein.jpg"
                     },
                     {
                         id: 2,
                         firstName: "R. L.",
                         lastName: "Stine",
-                        age: "76"
+                        age: "76",
+                        image: "https://m.media-amazon.com/images/M/MV5BNGEzZWI4YTMtYWIwNC00NDJmLWI0ZWEtOWUzODljNGE3ZDUwXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_SY1000_CR0,0,980,1000_AL_.jpg"
                     },
                     {
                         id: 3,
                         firstName: "Mike",
                         lastName: "Lupica",
-                        age: "67"
+                        age: "67",
+                        image: "http://www.mikelupicabooks.com/images/author_lupica.jpg"
                     },
                     {
                         id: 4,
                         firstName: "J. K.",
                         lastName: "Rowling",
-                        age: "54"
+                        age: "54",
+                        image: "https://www.dailysignal.com/wp-content/uploads/JKRowling-1250x650.jpg"
+                    },
+                    {
+                        id: 5,
+                        firstName: "Brian",
+                        lastName: "Selznick",
+                        age: "53",
+                        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/D03_9553_Brian_Selznick.jpg/1920px-D03_9553_Brian_Selznick.jpg"
+                    },
+                    {
+                        id: 6,
+                        firstName: "Stephen",
+                        lastName: "King",
+                        age: "72",
+                        image: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Stephen_King%2C_Comicon.jpg"
                     }
+
                 ]
         }
     };
-
     render(){
     return (
         <div className="Authors">
