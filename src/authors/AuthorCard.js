4b168c38d4054be6a3155bae6e03cb69c08bc616
Vinny Matticoli <Vinny@vinnys-mbp.home>
2020-02-26 02:56:32 -0500

    project init

--- src/authors/AuthorCard.js
+++ src/authors/AuthorCard.js
@@ -6,8 +6,8 @@ export function AuthorCard(props){
         <Card style={{width: "16em" }}>
             <Card.Img variant="top" src="https://via.placeholder.com/150"  />
             <Card.Body>
-                <Card.Title>{props.book.title}</Card.Title>
-                <Card.Text>Author: {props.book.author}</Card.Text>
+                <Card.Title>{props.author.firstName} {props.author.lastName} </Card.Title>
+                <Card.Text>Author Age: {props.author.age}</Card.Text>
                 <Button variant="warning">Add to Cart</Button>
             </Card.Body>
         </Card>
